1061ccebdbb785b3ee020b95804d85ecdcaa71a1
Rakshita <148205389+rakshita1357@users.noreply.github.com>
2026-04-13 23:25:20 +0530

    Time: 43 ms (42.78%) | Memory: 69.4 MB (94.97%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int count=0,new_n=0;
         for(int i=1;i<nums.size();++i){
-            if(nums[i-1]>=nums[i]){
+            if(nums[i]<=nums[i-1]){
                 new_n=nums[i-1]+1;
                 count+=new_n-nums[i];
                 nums[i]=new_n;
